50dfd9a9dfcae12805d5f1c463efe955b5bfbca3
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-02-24 15:58:02 +0900

    Initial commit

---
+++ src/server/index.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv');
+dotenv.config();
+// Require the Aylien npm package
+var aylien = require("aylien_textapi");
+
+// set aylien API credentials
+// NOTICE that textapi is the name I used, but it is arbitrary.
+// You could call it aylienapi, nlp, or anything else, 
+//   just make sure to make that change universally!
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
